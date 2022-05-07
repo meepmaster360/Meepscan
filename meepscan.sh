2c5b3fe10ef4429e148bada842534e8a736ea378
meepmaster360 <paulomotaborges@gmail.com>
2022-05-07 01:14:56 +0100

    4

--- meepscan.sh
+++ meepscan.sh
@@ -116,10 +116,10 @@ function nmap_ports_open_fast () {
 	read ip
 	echo -e "\n${GREEN}Grabbing open ports...${NOCOLOR}"
 	#ports=$(nmap -p- --min-rate 1000 -T4 $ip | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//) 
-	ports=$(nmap -p- -T4 $ip | grep open | cut -d "/" -f 1 | tr -s '\n' ',' | rev | cut -c 2- | rev) 
+	ports=`nmap -p- -T4 $ip | grep open | cut -d "/" -f 1 | tr -s '\n' ',' | rev | cut -c 2- | rev`
 	echo -e "\n${GREEN}Ports grabbed!${NOCOLOR}"
 	echo -e "\n${GREEN}Scanning open ports...${NOCOLOR}\n"
-	nmap -T4 -p $ports $ip
+	sudo nmap -T4 -p $ports $ip
 	echo				
 }
 
